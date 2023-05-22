42886941484c10f171664f2cdac602d700090e9b
mlazuardy <lazuardymichael@gmail.com>
2023-05-22 10:52:18 +0700

    chore: create a custom not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <p>Not Found</p>
+    </main>
+  );
+}
